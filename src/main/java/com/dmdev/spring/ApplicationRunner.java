a445bd6e7c9d3f412fc4b6a655f8002d9028ca7d
vafanasenka-frontlineed <vafanasenka@frontlineed.com>
2022-05-11 18:31:57 +0300

    prototype scope

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -7,11 +7,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 
-        var connectionPool = context.getBean("p1", ConnectionPool.class);
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-
-        System.out.println();
+            var connectionPool = context.getBean("p1", ConnectionPool.class);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        }
     }
 }
