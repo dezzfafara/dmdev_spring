10f38e301a70243a5008a7a27aa34f51b2710628
vafanasenka-frontlineed <vafanasenka@frontlineed.com>
2022-06-02 18:42:51 +0300

    @Autowired, @Value

--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -1,18 +1,26 @@
 package com.dmdev.spring.database.repository;
 
-import com.dmdev.spring.bpp.InjectBean;
 import com.dmdev.spring.bpp.Transaction;
 import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    @Autowired
+    private ConnectionPool pool1;
+
+    @Autowired
+    private List<ConnectionPool> pools;
+
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     private void init() {
